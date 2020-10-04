044d4414d1bc8498c96858d0f4f6ffb0754b3127
Jason Costomiris <jcostom@gandalf.jasons.org>
2020-10-03 23:35:24 -0400

    bump version

--- plugmon.py
+++ plugmon.py
@@ -22,7 +22,7 @@ def triggerWebHook():
         "with/key",
         IFTTTKEY)
     )
-    headers = {'User-Agent': 'plugmon.py v0.4'}
+    headers = {'User-Agent': 'plugmon.py v0.5'}
     response = requests.get(updateURL, headers=headers)
     print(time.strftime("[%d %b %Y %H:%M:%S]", time.localtime()) + " IFTTT Response: {}".format(response.text))
 
