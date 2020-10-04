a134fecd6590ac0a70bd33804bb723ae761af827
Jason Costomiris <jcostom@gandalf.jasons.org>
2020-10-03 23:03:25 -0400

    First Push

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM alpine:3.12
+
+ARG BUILD_DATE
+ARG VCS_REF
+
+LABEL org.label-schema.build-date=$BUILD_DATE \
+      org.label-schema.vcs-url="https://github.com/jcostom/dnsomatic-update.git" \
+      org.label-schema.vcs-ref=$VCS_REF \
+      org.label-schema.schema-version="1.0.0"
+
+VOLUME "/config"
+
+RUN \
+    apk add --no-cache py3-requests py3-pip \
+    && rm -rf /var/cache/apk/* \
+    && pip install pyvesync
+
+RUN mkdir /app
+COPY ./plugmon.py /app
+RUN chmod 755 /app/plugmon.py
+
+ENTRYPOINT [ "/app/plugmon.py" ]
---
+++ plugmon.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import requests
+from pyvesync import VeSync
+
+EMAIL = os.getenv('USERID')
+PASSWORD = os.getenv('PASSWORD')
+TZ = os.getenv('TZ', 'America/New_York')
+DEVID = os.getenv('DEVID', 0)
+IFTTTKEY = os.getenv('IFTTTKEY')
+IFTTTWEBHOOK = os.getenv('IFTTTWEBHOOK')
+
+INTERVAL = os.getenv('INTERVAL', 300)
+IS_RUNNING = 0
+
+def triggerWebHook():
+    webHookURL = "/".join(
+        ("https://maker.ifttt.com/trigger",
+        IFTTTWEBHOOK,
+        "with/key",
+        IFTTTKEY)
+    )
+    headers = {'User-Agent': 'plugmon.py v0.1'}
+    response = requests.get(updateURL, headers=headers)
+    print(time.strftime("[%d %b %Y %H:%M:%S]", time.localtime()) + " IFTTT Response: {}".format(response.text))
+
+def main():
+    manager = VeSync(EMAIL, PASSWORD, TZ)
+    manager.login()
+    manager.update()
+    mysw = manager.outlets[DEVID]
+    while True:    
+        manager.update()
+        manager.update_energy()
+        mysw_power = float(mysw.details.get('power',0))
+        if mysw_power > 0.5:
+            print(time.strftime("[%d %b %Y %H:%M:%S]", time.localtime()) + " Washer is running: {}".format(mysw_power))
+            IS_RUNNING = 1
+        else:
+            print(time.strftime("[%d %b %Y %H:%M:%S]", time.localtime()) + " Washer is stopped: {}".format(mysw_power))
+            triggerWebHook()
+            IS_RUNNING = 0
+
+        time.sleep(INTERVAL)
+
+if __name__ == "__main__":
+    main()
