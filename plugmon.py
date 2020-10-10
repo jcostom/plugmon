3d536c8b93bd4cff9d3123b40495b334378e553e
Jason Costomiris <jcostom@jasons.org>
2020-10-09 20:16:24 -0400

    Initial Push

--- plugmon.py
+++ plugmon.py
@@ -3,19 +3,68 @@
 import os
 import time
 import requests
+import random
+import hashlib
 from pyvesync_v2 import VeSync
 
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASSWORD')
+MD5PASSWORD = hashlib.md5(PASSWORD.encode('utf-8')).hexdigest()
 TZ = os.getenv('TZ', 'America/New_York')
-DEVID = int(os.getenv('DEVID', 0))
-OFFPOWER = float(os.getenv('OFFPOWER', 0.2))
-ONPOWER = float(os.getenv('ONPOWER', 0.8))
-
+DEVNAME = os.getenv('DEVNAME')
+OFFPOWER = float(os.getenv('OFFPOWER', 1.2))
+ONPOWER = float(os.getenv('ONPOWER', 3.0))
 IFTTTKEY = os.getenv('IFTTTKEY')
 IFTTTWEBHOOK = os.getenv('IFTTTWEBHOOK')
-
 INTERVAL = os.getenv('INTERVAL', 300)
+TRACEID = random.uniform(0, 1)
+
+def findDeviceID(deviceName, email, md5pass, tz, traceid):
+    # First fetch token & accountID
+    headers = { 'content-type': 'application/json' }
+    body = {
+        'accountID': '',
+        'account': email,
+        'password': md5pass,
+        'timeZone': tz,
+        'token': ''
+    }
+    url = 'https://smartapi.vesync.com/vold/user/login'
+    r = requests.post(url, headers=headers, json=body)
+    ACCOUNTID = r.json()['accountID']
+    TOKEN = r.json()['tk']
+
+    # Next collect list of devices
+    headers = {
+      'tk': TOKEN,
+      'accountid': ACCOUNTID,
+      'content-type': 'application/json',
+      'tz': tz,
+      'user-agent': 'HappyFunBall'
+    }
+    body = {
+        'acceptLanguage': 'en',
+        'accountID': ACCOUNTID,
+        'appVersion': '1.1',
+        'method': 'devices',
+        'pageNo': '1',
+        'pageSize': '1000',
+        'phoneBrand': 'HappyFunBall',
+        'phoneOS': 'HappyFunOS',
+        'timeZone': tz,
+        'token': TOKEN,
+        'traceId': traceid
+    }
+    url = 'https://smartapi.vesync.com/cloud/v2/deviceManaged/devices'
+    r = requests.post(url, headers=headers, json=body)
+    list = r.json()['result']['list']
+
+    for i in range(len(list)):
+    if list[i]['deviceName'] == devName:
+        devID = i
+        break
+    
+    return(devID)
 
 def triggerWebHook():
     webHookURL = "/".join(
@@ -24,11 +73,17 @@ def triggerWebHook():
         "with/key",
         IFTTTKEY)
     )
-    headers = {'User-Agent': 'plugmon.py v1.1'}
-    response = requests.get(webHookURL, headers=headers)
-    print(time.strftime("[%d %b %Y %H:%M:%S %Z]", time.localtime()) + " IFTTT Response: {}".format(response.text))
+    headers = {'User-Agent': 'plugmon.py v1.5'}
+    r = requests.get(webHookURL, headers=headers)
+    print(time.strftime("[%d %b %Y %H:%M:%S %Z]", time.localtime()) + " IFTTT Response: {}".format(r.text))
 
 def main():
+    DEVID = findDeviceID(DEVICENAME, EMAIL, MD5PASSWORD, TZ, TRACEID)
+
+    # Now, get down to business, finally.
+    # Hey VeSync gang - give us a way to directly access
+    # info by UUID or CID fields!
+
     manager = VeSync(EMAIL, PASSWORD, TZ)
     manager.login()
     manager.update()
