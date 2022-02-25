8376989a3fb2dc5fc58c40d863b73e090ddf3277
Jason Costomiris <jcostom@jasons.org>
2022-02-25 00:10:15 -0500

    update version

--- plugmon.py
+++ plugmon.py
@@ -19,7 +19,7 @@ IFTTTWEBHOOK = os.getenv('IFTTTWEBHOOK')
 INTERVAL = os.getenv('INTERVAL', 300)
 TRACEID = str(random.uniform(1, 1000000000))
 
-VER = "2.6"
+VER = "2.8.1"
 USER_AGENT = "plugmon.py/" + VER
 
 
