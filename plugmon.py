cedea87b4d6033d2f5952a0f48db3d2e69a15cb4
Jason Costomiris <jcostom@jasons.org>
2022-04-18 10:28:46 -0400

    BIG autobuild update

--- plugmon.py
+++ plugmon.py
@@ -22,7 +22,7 @@ CHATID = int(os.getenv('CHATID'))
 MYTOKEN = os.getenv('MYTOKEN')
 DEBUG = int(os.getenv('DEBUG', 0))
 
-VER = "3.4"
+VER = "3.4.1"
 USER_AGENT = f"plugmon.py/{VER}"
 
 # Setup logger
