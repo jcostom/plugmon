7998dad116721d3cb4d8f488156506b443242272
Jason Costomiris <jcostom@jasons.org>
2023-02-08 18:44:58 -0500

    fix version number

--- plugmon.py
+++ plugmon.py
@@ -28,7 +28,7 @@ DEBUG = int(os.getenv('DEBUG', 0))
 TRACEID = str(random.uniform(1, 1000000000))
 MD5PASSWORD = hashlib.md5(PASSWORD.encode('utf-8')).hexdigest()
 
-VER = "3.6"
+VER = "3.6.1"
 USER_AGENT = f"plugmon.py/{VER}"
 
 # Setup logger
